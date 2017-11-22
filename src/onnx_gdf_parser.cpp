813835341fc54c7ba847bbdd8ed09780d501b0c1
lcskrishna <lollachaitanya@gmail.com>
2017-11-21 17:01:27 -0800

    Extraction of weights from onnx model

--- src/onnx_gdf_parser.cpp
+++ src/onnx_gdf_parser.cpp
@@ -12,12 +12,121 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+void formatFileName(std::string& str, const std::string& from, const std::string& to)
+{
+    //Written to avoid conflicts with file creation with filenames that contain "/"
+    size_t start_pos = 0;
+    while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
+    }
+}
+
+int dumpOnnxModel(const onnx::GraphProto& graph_proto)
+{
+	for(int i=0; i < graph_proto.initializer_size(); i++) 
+	{
+		const onnx::TensorProto& tensor_proto = graph_proto.initializer(i);
+		const onnx::TensorProto_DataType& datatype = tensor_proto.data_type();
+		int tensor_size  = 1;
+	
+		for(int j = 0; j < tensor_proto.dims_size(); j++) {
+			tensor_size *= tensor_proto.dims(j);
+		}
+
+		std::string weight_file_name = tensor_proto.name();
+		formatFileName(weight_file_name, "/", "_");
+		std::string fileName_weights = "weights/" + weight_file_name + ".f32";
+
+		if(datatype == onnx::TensorProto_DataType_FLOAT) {
+			
+			FILE * fs;
+			fs = fopen(fileName_weights.c_str(), "wb");
+			if(!fs) {
+				std::cout << "ERROR: Unable to create a file, make sure weights folder is writable." << std::endl;
+				return -1;
+			}
+			std::string raw_data_val = tensor_proto.raw_data();
+			const char * val = raw_data_val.c_str();
+			
+			int count = 0;
+			for(int k = 0; k < tensor_size*4 - 4; k+=4) {
+				float weight;
+				char b[] = {val[k], val[k+1], val[k+2], val[k+3]};
+				memcpy(&weight, &b, sizeof(float));
+				fwrite(&weight, sizeof(float), 1, fs);
+				count++;
+			}
+		
+			fclose(fs);
+			std::cout << "INFO: Weights dumped for: " << tensor_proto.name() <<  std::endl;
+		}
+		else {
+			std::cout <<"ERROR: Unsupported data types will be supported in future." << std::endl;
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
 int parseOnnxGraph(const onnx::GraphProto& graph_proto)
 {
 	if(graph_proto.has_name()) {
-		std::cout << "INFO: Creating a network structure for : " << graph_proto.name() << std::endl;
+		std::cout << "INFO: Extracting the weights for : " << graph_proto.name() << std::endl;
 	}
 
+	if(dumpOnnxModel(graph_proto) < 0) {
+		std::cout << "ERROR: Unable to dump weights from onnx model. " << std::endl;
+		return -1;
+	}
+	else {
+		std::cout << "RESULT: Weights and bias extraction successful" << std::endl;
+	}
+
+	/*
+	for(int i=0; i < graph_proto.initializer_size(); i++) {
+		const onnx::TensorProto& tensor_proto = graph_proto.initializer(i);
+		const onnx::TensorProto_DataType& datatype = tensor_proto.data_type();
+		int tensor_size  = 1;
+		for(int j = 0; j < tensor_proto.dims_size(); j++) {
+			tensor_size *= tensor_proto.dims(j);
+			std::cout << tensor_proto.dims(j) << std::endl;
+		}
+
+		std::string fileName_weights = "weights/" + tensor_proto.name() + ".f32";
+ 
+		if(datatype == onnx::TensorProto_DataType_FLOAT) {
+			
+			FILE * fs;
+			fs = fopen(fileName_weights.c_str(), "wb");
+			if(!fs) {
+				std::cout << "ERROR: Unable to create a file. " << std::endl;
+			}
+			std::string raw_data_val = tensor_proto.raw_data();
+			const char * val = raw_data_val.c_str();
+			
+			int count = 0;
+			for(int k = 0; k < tensor_size*4 - 4; k+=4) {
+				float weight;
+				char b[] = {val[k], val[k+1], val[k+2], val[k+3]};
+				memcpy(&weight, &b, sizeof(float));
+				fwrite(&weight, sizeof(float), 1, fs);
+				count++;
+			}
+		
+			fclose(fs);
+			std::cout << "INFO: Weights are dumped into the weights folder" << std::endl;
+		}
+		else {
+			std::cout <<"ERROR: Unable to extract the data. Will be supported in future." << std::endl;
+		}
+		
+	}*/
+
+	
+
+	/*
 	for(int i=0; i < graph_proto.node_size(); i++) {
 		const onnx::NodeProto node_proto = graph_proto.node(i);
 		std::cout << "INFO: Layer is : " << node_proto.op_type() << std::endl;
@@ -29,9 +138,21 @@ int parseOnnxGraph(const onnx::GraphProto& graph_proto)
 		for(int j=0; j < node_proto.output_size(); j++) {
 			std::cout << "Output is : " << node_proto.output(j) << std::endl;
 		}
-	}
 
-	return 1;
+		std::cout << "INFO: attribute proto size is : " << node_proto.attribute_size() << std::endl;
+		if(node_proto.attribute_size() > 0) {
+			
+			for(int j=0; j < node_proto.attribute_size() ; j++) {
+				const onnx::AttributeProto& attribute_proto = node_proto.attribute(j);
+				std::cout << "Attribute name is: " << attribute_proto.name() << std::endl;
+				std::cout << "Tensors size is : " << attribute_proto.tensors_size() << std::endl;
+			}
+			
+		}
+	}
+	*/
+
+	return 0;
 }
 
 int loadOnnxModelFile(const char * fileName)
